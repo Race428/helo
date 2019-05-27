9709386cc4422240f0f5b224a894111662039c6a
Race428 <raceacheson@gmail.com>
2019-05-27 15:41:55 -0600

    button

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,11 +2,32 @@ import React, {Component} from 'react'
 
 
 export default class Dashboard extends Component { 
+   constructor(){
+       super()
+   
+    this.state = { 
+        myPostsChecked: true,
+        
+    }
+   }
 
 
+    myPostsChecked = () => { 
+        this.setState({
+            myPostsChecked: !this.state.myPostsChecked
+        })
+    }
+    
     render() { 
         return(
-            <div>This is Dashboard</div>
+            <div>This is Dashboard
+            <input placeholder= 'search'/> 
+            <label> My Posts
+            <input 
+            onClick={this.myPostsChecked}
+            type = 'checkbox' checked={this.state.myPostsChecked} />
+            </label>
+            </div>
         )
     }
 }
