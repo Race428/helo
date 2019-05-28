83a4d47b1a59d8b631beca6c2f896388f6c75849
Race428 <raceacheson@gmail.com>
2019-05-28 15:21:53 -0600

    added user filter

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -21,104 +21,124 @@ class Dashboard extends Component {
 
 
     async componentDidMount() {
-
-        await  axios.get('/getposts').then(res => {
+        console.log(this.props.match)
+        await axios.get('/getposts').then(res => {
             this.setState({
                 posts: res.data
             })
         })
     }
-        // if (this.state.myPostsChecked === false) {
-        //     const { user_id } = this.state
-        //    await  axios.get('/getnouserposts', { user_id }).then(res => {
+    // if (this.state.myPostsChecked === false) {
+    //     const { user_id } = this.state
+    //    await  axios.get('/getnouserposts', { user_id }).then(res => {
+    //         this.setState({
+    //             posts: res.data
+    //         })
+    //     })
+    // }
+
+    // else {
+    //     if (this.state.myPostsChecked === true) {
+    //         await  axios.get('/getposts').then(res => {
+    //             this.setState({
+    //                 posts: res.data
+    //             })
+    //         })
+    //     }
+    // }
+
+
+    myPostsChecked = () => {
+        this.setState({
+            myPostsChecked: !this.state.myPostsChecked
+        })
+
+
+        // if (this.state.myPostsChecked === true) {
+        //      axios.get('/getposts').then(res => {
         //         this.setState({
         //             posts: res.data
         //         })
         //     })
         // }
-
         // else {
-        //     if (this.state.myPostsChecked === true) {
-        //         await  axios.get('/getposts').then(res => {
-        //             this.setState({
-        //                 posts: res.data
-        //             })
+        //     const { user_id } = this.state
+        //      axios.get('getnouserposts', { user_id }).then(res => {
+        //         this.setState({
+        //             posts: res.data
         //         })
-        //     }
+        //     })
         // }
-        
-
-        myPostsChecked = () => {
-            this.setState({
-                myPostsChecked: !this.state.myPostsChecked
-            })
-      
-
-            // if (this.state.myPostsChecked === true) {
-            //      axios.get('/getposts').then(res => {
-            //         this.setState({
-            //             posts: res.data
-            //         })
-            //     })
-            // }
-            // else {
-            //     const { user_id } = this.state
-            //      axios.get('getnouserposts', { user_id }).then(res => {
-            //         this.setState({
-            //             posts: res.data
-            //         })
-            //     })
-            // }
-        }
+    }
 
 
-        searchChange(value){
-            this.setState({
-                search: value
-            })
-        }
-
-        render() {
-            
-            const posts = this.state.posts.filter((element) => {
-                return element.title.includes(this.state.search)
-            }).map((element) => { 
-                return   <div
-                key={element.id}>
-                {/* <img src={`${element.posts.profile_pic}`} alt='profile' /> */}
-                <h1>{element.username}</h1>
-                <h1>{element.title}</h1>
-
-            </div>
+    searchChange(value) {
+        this.setState({
+            search: value
         })
-            
-                
-                
-              
-            return (
-                <div>This is Dashboard
-            <input onChange = {e => this.searchChange(e.target.value)}
-            placeholder='search' />
-                    <label> My Posts
-            <input
-                            onClick={this.myPostsChecked}
-                            type='checkbox' checked={this.state.myPostsChecked} />
-                    </label>
-                    {posts}
+    }
 
+
+
+
+    render() {
+        if (this.state.myPostsChecked === true) {
+            var posts = this.state.posts.filter((element) => {
+                console.log(element)
+                return element.title.includes(this.state.search)
+            }).map((element) => {
+                return <div
+                    key={element.id}>
+                    {/* <img src={`${element.posts.profile_pic}`} alt='profile' /> */}
+                    <h1>{element.username}</h1>
+                    <h1>{element.title}</h1>
 
                 </div>
-            )
+            })
         }
-    }
+
+        else {
+            var posts = this.state.posts.filter((element) => {
+                console.log(element)
+                return element.title.includes(this.state.search)
+            }).map((element) => {
+                if(element.author_id !== this.state.user_id){
+                return <div
+                    key={element.id}>
+                    {/* <img src={`${element.posts.profile_pic}`} alt='profile' /> */}
+                    <h1>{element.username}</h1>
+                    <h1>{element.title}</h1>
+
+                </div>
+            }})
+            
+            
+        }
+
+        return (
+            <div>This is Dashboard
+            <input onChange={e => this.searchChange(e.target.value)}
+                    placeholder='search' />
+                <label> My Posts
+            <input
+                        onClick={this.myPostsChecked}
+                        type='checkbox' checked={this.state.myPostsChecked} />
+                </label>
+                {posts}
 
 
-    const mapStateToProps = (reduxState) => {
-        console.log(reduxState)
-        const { userObj } = reduxState
-        console.log(typeof userObj)
-        return {
-            userObj
-        }
+            </div>
+        )
     }
-    export default connect(mapStateToProps)(Dashboard)
+}
+
+
+const mapStateToProps = (reduxState) => {
+    console.log(reduxState)
+    const { userObj } = reduxState
+    console.log(typeof userObj)
+    return {
+        userObj
+    }
+}
+export default connect(mapStateToProps)(Dashboard)
